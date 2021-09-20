dbff5ed8371b2274ab090dcec7b1e26a24272f84
zxq1998 <1185209862@qq.com>
2021-09-20 15:32:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/
+cd /package/lean/  
+
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+
+git clone https://github.com/jerrykuku/luci-app-vssr.git
